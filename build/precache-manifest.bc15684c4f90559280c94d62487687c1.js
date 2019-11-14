3f51ba01987355cc0e9c6b0a18917fe14e461872
Daniel Lobo <info@danlobo.co.uk>
2019-11-14 18:14:14 +0000

    - add build

---
+++ build/precache-manifest.bc15684c4f90559280c94d62487687c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec5bbd9dda49a9e6d3f945d876eefac0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1312dbd5e41cb764829",
+    "url": "/static/css/main.7744e8cb.chunk.css"
+  },
+  {
+    "revision": "b970087179f992a78ea1",
+    "url": "/static/js/2.05d54327.chunk.js"
+  },
+  {
+    "revision": "d1312dbd5e41cb764829",
+    "url": "/static/js/main.b446bd15.chunk.js"
+  },
+  {
+    "revision": "38000720b1148f379526",
+    "url": "/static/js/runtime-main.4e33dd35.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
