2599f2bfc6b4f797ef0cc15d01c6939af9ac7fb5
Daniel Lobo <info@danlobo.co.uk>
2019-11-14 18:40:11 +0000

    - add new build

---
+++ build/precache-manifest.17f48069a39cd8fb7db823cc3ba7dce3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "352fa2070042691fdcc64a8e01362a21",
+    "url": "/tetris/build/index.html"
+  },
+  {
+    "revision": "9391c077f454b628651a",
+    "url": "/tetris/build/static/css/main.7744e8cb.chunk.css"
+  },
+  {
+    "revision": "b970087179f992a78ea1",
+    "url": "/tetris/build/static/js/2.05d54327.chunk.js"
+  },
+  {
+    "revision": "9391c077f454b628651a",
+    "url": "/tetris/build/static/js/main.349c8aab.chunk.js"
+  },
+  {
+    "revision": "402e0ff1f3a4207cc1c5",
+    "url": "/tetris/build/static/js/runtime-main.19567463.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tetris/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
